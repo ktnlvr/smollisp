eec5aa92b6ec86a61c824fada7a249a2fd5ee935
Kittenlover229 <artur.roos@mail.ru>
2022-05-01 10:38:20 +0000

    feat: Get some deserved formatting for the Value

--- ext/lldb/type_summaries.py
+++ ext/lldb/type_summaries.py
@@ -1,3 +1,25 @@
 import lldb
 
-print("Hello, world!")
+
+def __lldb_init_module(debugger, dict):
+    print("Loaded extension for smollisp")
+    debugger.HandleCommand(
+        'type summary add -F type_summaries.ValueSummary smollisp_Value')
+
+
+def ValueSummary(value: lldb.SBValue, _):
+    kind: lldb.SBValue = value.GetChildMemberWithName("kind")
+    kind: str = kind.GetValue()
+    if kind == "SMOLLISP_VALUE_KIND_INT32":
+        int32: lldb.SBValue = value.GetChildMemberWithName("int32")
+        int32 = int32.GetValueAsSigned()
+        return f"(int32 {int32})"
+    elif kind == "SMOLLISP_VALUE_KIND_NONE":
+        return "(none)"
+    elif kind == "SMOLLISP_VALUE_KIND_STRING":
+        string: lldb.SBValue = value.GetChildMemberWithName(
+            "string").GetChildMemberWithName("buf")
+        string = string.GetSummary()
+        return f"(string {string})"
+    else:
+        return "(<invalid>)"
